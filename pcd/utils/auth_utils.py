39352aa2d566270abb07beb675c645c3fc8824d4
chenfx <chenfx@inspur.com>
2023-04-24 16:46:44 +0800

    update auth

--- pcd/utils/auth_utils.py
+++ pcd/utils/auth_utils.py
@@ -15,7 +15,7 @@ class AuthRequestUtil(HTTPRequestUtil):
 
     def __is_successful_response(self, result):
         try:
-            return result['authenticated'] == True and result['currentUser']['info']['email'] != None and result['currentUser']['info']['email'] != ''
+            return result['authenticated'] == True and result['currentUser']['email'] != None and result['currentUser']['email'] != ''
         except Exception:
             return False
 
@@ -26,37 +26,25 @@ class AuthRequestUtil(HTTPRequestUtil):
 
             '''
             {
-                "jsessionid": "48a39a23-58c0-4cb1-b8cc-10d2ce52a301",
+                "jsessionid": "f45cde6c-49ba-44b9-8b50-8e1bc886f968",
                 "currentUser": {
-                    "username": "zhangs12",
-                    "nickname": "张三",
-                    "phone": "",
+                    "username": "ygtest003",
+                    "nickname": "测试员工3",
+                    "phone": "18612345678",
                     "telephone": "",
-                    "email": "",
+                    "email": "ygtest003@chinaunicom.cn",
+                    "info": null,
                     "birthday": null,
-                    "sex": 1,
-                    "info": {
-                        "code": "0980870",
-                        "name": "张三",
-                        "mobile": "18510369819",
-                        "userName": "zhangs12",
-                        "email": "zhangs12@chinaunicom.cn",
-                        "gender": "M",
-                        "birthday": "1992/3/13",
-                        "post": "",
-                        "dptName": "智能技术研究部-科研环境",
-                        "dptCode": "830247140",
-                        "type": "1"
-                    }
+                    "sex": 0
                 },
-                "timeout": 1800000,
+                "timeout": 18000000,
                 "authenticated": true
             }
             '''
             if (result == None or not self.__is_successful_response(result)):
                 return None
             Log.info('got result is {}'.format(json.dumps(result)))
-            return User.objects.filter(username=result['currentUser']['info']['userName'], email=result['currentUser']['info']['email']).last()
+            return User.objects.filter(username=result['currentUser']['username'], email=result['currentUser']['email']).last()
         except Exception as err:
             Log.exception('get_user err:[' + str(err) + ']')
         return None
